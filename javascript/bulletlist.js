2ec86e1832a699d9447a9b84ac15079896027507
Luke Merrett <lukeamerrett@gmail.com>
2012-04-05 16:47:02 +0100

    Added methods for adding a new bullet point when pressing enter

--- javascript/bulletlist.js
+++ javascript/bulletlist.js
@@ -5,10 +5,13 @@ JSBulletList = function($) {
 	
 	var bulletListSuffix = "_BulletList";
 	var bulletSuffix = "_Bullet_";
+	var inputSuffix = "_Input_";
 	
 	// Adds a new editable bullet point list to a placeholder
 	bulletList.setupNewBulletList = function(containingDivId) {
 		$('#'+containingDivId).html(newBulletListHtml(containingDivId));
+		
+		addEnterKeyEventHandlerToBulletPoints(containingDivId);
 	}
 	
 	// Focuses the cursor on the first bullet point in the list
@@ -21,7 +24,27 @@ JSBulletList = function($) {
 	}
 	
 	function newBulletPointHtml(idPrefix, bulletPointNumber, currentText) {
-		return "<li id='"+idPrefix+bulletSuffix+bulletPointNumber+"'><input type='text'>"+currentText+"</input></li>";
+		return "<li id='"+idPrefix+bulletSuffix+bulletPointNumber+"'>"+
+					"<input type='text' id='"+idPrefix+inputSuffix+bulletPointNumber+"' name='"+bulletPointNumber+"'>"+currentText+"</input>"+
+				"</li>";
+	}
+	
+	function addEnterKeyEventHandlerToBulletPoints(idPrefix) {
+		$('#'+idPrefix+" ul").on("keyup", "li input", function(event){
+				if (event.which == 13) {
+					handleEnterKeyOnBulletPoint(event.srcElement.id);
+				}
+			});
+	}
+	
+	function handleEnterKeyOnBulletPoint(inputBoxId) {
+		var currentListItem = $('#'+inputBoxId).parent();
+		var currentNumber = parseInt($('#'+inputBoxId).attr("name"));
+		var newNumber = currentNumber + 1
+		var idPrefix = currentListItem.parent().parent().attr("id");
+		
+		currentListItem.append(newBulletPointHtml(idPrefix, newNumber, ""));
+		setFocusOnSpecificBulletPoint(idPrefix, newNumber);
 	}
 	
 	function setFocusOnSpecificBulletPoint(idPrefix, bulletPointNumber) {
