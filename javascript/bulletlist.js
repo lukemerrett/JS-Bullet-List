c1d6629cf77a9a51716c8fbdad30b314f3448a3f
Luke Merrett <lukeamerrett@gmail.com>
2012-04-05 15:38:11 +0100

    Added method to select the first bullet point on a list

--- javascript/bulletlist.js
+++ javascript/bulletlist.js
@@ -3,14 +3,24 @@ var JSBulletList = {};
 JSBulletList = function($) {
 	var bulletList = {};
 	
+	// Adds a new editable bullet point list to a placeholder
 	bulletList.setupNewBulletList = function(containingDivId) {
-		$('#'+containingDivId).html(newBulletListHtml());
+		$('#'+containingDivId).html(newBulletListHtml(containingDivId));
 	}
 	
-	function newBulletListHtml() {
-		var html = "<ul class='jsBulletList'><li><input type='text'></input></li></ul>";
+	// Focuses the cursor on the first bullet point in the list
+	bulletList.setFocusOnFirstBulletPoint = function(containingDivId) {
+		$('#'+containingDivId+" ul li:first-child input").focus();
+	}
+	
+	function newBulletListHtml(idPrefix) {
+		var firstBulletPoint = newBulletPointHtml(idPrefix, 1, "");
 		
-		return html;
+		return "<ul id='"+idPrefix+"_BulletList' class='jsBulletList'>"+firstBulletPoint+"</ul>";
+	}
+	
+	function newBulletPointHtml(idPrefix, bulletPointNumber, currentText) {
+		return "<li id='"+idPrefix+"_Bullet_"+bulletPointNumber+"'><input type='text'>"+currentText+"</input></li>";
 	}
 	
 	return bulletList;
