cfc47f66afc7a47d719b622552353c5874d42269
Luke Merrett <lukeamerrett@gmail.com>
2012-04-05 14:54:43 +0100

    Added placeholder div to demo and the setup method to the javascript

--- javascript/bulletlist.js
+++ javascript/bulletlist.js
@@ -3,7 +3,13 @@ var JSBulletList = {};
 JSBulletList = function($) {
 	var bulletList = {};
 	
+	bulletList.setupNewBulletList = function(containingDivId) {
+		$('#'+containingDivId).html(newBulletListHtml());
+	}
 	
+	function newBulletListHtml() {
+		// TODO
+	}
 	
 	return bulletList;
 }(jQuery);
