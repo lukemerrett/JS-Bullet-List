8b3f26f985fb3c5b84e16968a25c64d26328a654
Luke Merrett <lukeamerrett@gmail.com>
2012-04-06 18:58:10 +0100

    Added a model to the javascript. Refactored code to allow for an incrementing unique id whilst using the array inside the model for the ordering of the new items

--- javascript/bulletlist.js
+++ javascript/bulletlist.js
@@ -1,7 +1,51 @@
+
+// Holds details on a single bullet point
+function BulletPointObject(inputBoxId) {
+	
+	// The client id of the input box this object refers to
+	this.inputBoxId = inputBoxId;
+}
+
+// Holds details on all the bullet points within a group
+function BulletListModel() {
+
+	// Current unique identifier for all bullet points
+	this.currentUid = -1;
+
+	// Holds all the bullet points in the group
+	this.bulletPoints = [];
+}
+
+BulletListModel.prototype.addBulletPoint = function(position, bulletPointObject) {
+	// Add at a specific index to preserve ordering
+    this.bulletPoints.splice(position, 0, bulletPointObject);
+};
+
+BulletListModel.prototype.getBulletPointByIndex = function(position) {
+	return this.bulletPoints[position];
+}
+
+BulletListModel.prototype.getBulletPointIndexByInputBoxId = function(inputBoxId) {
+	var bulletPoint = $.grep(this.bulletPoints, function(value,index){
+		return value.inputBoxId == inputBoxId;
+	})[0];
+	
+	return $.inArray(bulletPoint, this.bulletPoints);
+}
+
+BulletListModel.prototype.removeBulletPoint = function(position) {
+	this.bulletPoints.splice(position, 1);
+};
+
 var JSBulletList = {};
 
 JSBulletList = function($) {
 	var bulletList = {};
+	
+	// Holds the models defining the bullet list
+	// There may be multiple lists on a page so we must hold them 
+	// in an object with the containing div id used as they key
+	var bulletListModels = {};
 	
 	var bulletListSuffix = "_BulletList";
 	var bulletSuffix = "_Bullet_";
@@ -12,6 +56,11 @@ JSBulletList = function($) {
 	
 	// Adds a new editable bullet point list to a placeholder
 	bulletList.setupNewBulletList = function(containingDivId) {
+	
+		// Setup the model
+		bulletListModels[containingDivId] = new BulletListModel();
+	
+		// Add the containing html and first bullet point
 		$('#'+containingDivId).html(newBulletListHtml(containingDivId));
 		
 		addKeyEventHandlersToBulletPoints(containingDivId);
@@ -19,16 +68,31 @@ JSBulletList = function($) {
 	
 	// Focuses the cursor on the first bullet point in the list
 	bulletList.setFocusOnFirstBulletPoint = function(containingDivId) {
-		setFocusOnSpecificBulletPoint(containingDivId, 1);
+		var bulletPoint = bulletListModels[containingDivId].getBulletPointByIndex(0);
+		setFocusOnSpecificBulletPoint(bulletPoint.inputBoxId);
 	}
 	
 	function newBulletListHtml(idPrefix) {		
-		return "<ul id='"+idPrefix+bulletListSuffix+"' class='jsBulletList'>"+newBulletPointHtml(idPrefix, 1, "")+"</ul>";
+		return "<ul id='"+idPrefix+bulletListSuffix+"' class='jsBulletList'>"+
+					newBulletPointHtml(idPrefix, 0)+
+				"</ul>";
 	}
 	
-	function newBulletPointHtml(idPrefix, bulletPointNumber, currentText) {
+	function newBulletPointHtml(idPrefix, position) {
+	
+		bulletListModels[idPrefix].currentUid++;
+		
+		var bulletPointNumber = bulletListModels[idPrefix].currentUid;
+		
+		var inputBoxId = idPrefix+inputSuffix+bulletPointNumber;
+		
+		// Add bullet point to the model
+		var newPoint = new BulletPointObject(inputBoxId);
+		bulletListModels[idPrefix].addBulletPoint(position, newPoint);
+	
 		return "<li id='"+idPrefix+bulletSuffix+bulletPointNumber+"'>"+
-					"<input type='text' id='"+idPrefix+inputSuffix+bulletPointNumber+"' name='"+bulletPointNumber+"'>"+currentText+"</input>"+
+					"<input type='text' id='"+inputBoxId+"' name='"+bulletPointNumber+"'>"+
+					"</input>"+
 				"</li>";
 	}
 	
@@ -46,12 +110,16 @@ JSBulletList = function($) {
 	// Adds a new bullet point under the existing bullet point when pressing enter
 	function handleEnterKeyOnBulletPoint(inputBoxId) {
 		var currentListItem = $('#'+inputBoxId).parent();
-		var currentNumber = parseInt($('#'+inputBoxId).attr("name"));
-		var newNumber = currentNumber + 1
 		var idPrefix = currentListItem.parent().parent().attr("id");
 		
-		currentListItem.after(newBulletPointHtml(idPrefix, newNumber, ""));
-		setFocusOnSpecificBulletPoint(idPrefix, newNumber);
+		var bulletPointIndex = bulletListModels[idPrefix].getBulletPointIndexByInputBoxId(inputBoxId);
+		var newIndex = bulletPointIndex + 1;
+		
+		currentListItem.after(newBulletPointHtml(idPrefix, newIndex));
+		
+		var bulletPointToSwitchTo = bulletListModels[idPrefix].getBulletPointByIndex(newIndex);
+		
+		setFocusOnSpecificBulletPoint(bulletPointToSwitchTo.inputBoxId);
 	}
 	
 	// Deletes the current bullet point if backspace is pressed when there is no text left in the bullet point
@@ -60,18 +128,23 @@ JSBulletList = function($) {
 		
 		if (inputBoxTextLength == 0) {
 			var currentListItem = $('#'+inputBoxId).parent();
-			var currentNumber = parseInt($('#'+inputBoxId).attr("name"));
-			var numberToSwitchFocusTo = currentNumber - 1;
 			var idPrefix = currentListItem.parent().parent().attr("id");
-			
 			currentListItem.remove();
-			setFocusOnSpecificBulletPoint(idPrefix, numberToSwitchFocusTo);
+			
+			var bulletPointIndex = bulletListModels[idPrefix].getBulletPointIndexByInputBoxId(inputBoxId);
+
+			var bulletPointToSwitchTo = bulletListModels[idPrefix].getBulletPointByIndex(bulletPointIndex - 1);
+			
+			setFocusOnSpecificBulletPoint(bulletPointToSwitchTo.inputBoxId);
+			
+			bulletListModels[idPrefix].removeBulletPoint(bulletPointIndex);
 		}
 	}
 	
-	function setFocusOnSpecificBulletPoint(idPrefix, bulletPointNumber) {
-		$('#'+idPrefix+bulletSuffix+bulletPointNumber+" input").focus();
+	function setFocusOnSpecificBulletPoint(inputBoxId) {
+		$('#'+inputBoxId).focus();
 	}
 	
 	return bulletList;
 }(jQuery);
+
